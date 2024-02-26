c39c8b265013cd17a6f04427cbbda531e15393ba
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-26 21:02:27 +0700

    updateTodo with change Completed status, set State todoList and refresh display, axios request PUT.

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -16,6 +16,26 @@ const TodoList = () => {
     navigate(`/todo/${id}`)
   };
 
+  const handleUpdate = async (id) => {
+    try {
+
+      await axios.put(`https://dummyjson.com/todos/${id}`, { completed: !todoList.find(todo => todo.id === id).completed }).then(success => console.log(success, "Success"));
+
+      //Updating a todo will not update it into the server =>
+
+      const updatedTodoList = todoList.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+
+      setTodosList(updatedTodoList);
+    } catch (error) {
+      console.error('Error update: ', error);
+    }
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
@@ -25,7 +45,34 @@ const TodoList = () => {
             <div>
               <p style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.todo}</p>
               <p>Completed: {todo.completed ? 'Yes' : 'No'}</p>
-              <button onClick={() => handleDetail(todo.id)}>Detail</button>
+              <button
+                style={{ marginRight: '10px', border: '1px solid #000000', borderRadius: '8px', cursor: 'pointer' }}
+                onClick={() => handleDetail(todo.id)}
+                onMouseEnter={(e) => {
+                  e.target.style.backgroundColor = '#491919';
+                  e.target.style.color = '#ffffff';
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.backgroundColor = '#ffffff';
+                  e.target.style.color = '#000000';
+                }}
+              >
+                Detail
+              </button>
+              <button
+                style={{ marginRight: '10px', border: '1px solid #000000', borderRadius: '8px', cursor: 'pointer' }}
+                onClick={() => handleUpdate(todo.id)}
+                onMouseEnter={(e) => {
+                  e.target.style.backgroundColor = '#491919';
+                  e.target.style.color = '#ffffff';
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.backgroundColor = '#ffffff';
+                  e.target.style.color = '#000000';
+                }}
+              >
+                Update
+              </button>
             </div>
           </li>
         ))}
